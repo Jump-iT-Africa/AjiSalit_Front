9c920f68d43d591d78b2b3c52de27e9917ce1dc2
OSMaben <freegiftcardishere@gmail.com>
2025-05-06 17:18:01 +0100

    i changed on the metro config

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,7 @@
 const { getDefaultConfig } = require('@expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
+defaultConfig.resolver.assetExts.push("hcscript");
+defaultConfig.transformer.assetPlugins = ["expo-asset/tools/hashAssetFiles"];
 
 module.exports = config;
